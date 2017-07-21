ac098fadf067bf44b69ab1715ebaeb71e9daccfb
churui <churui369@gmail.com>
2017-07-22 01:02:44 +0800

    11

--- src/main/java/com/jiaojing/L011/Test.java
+++ src/main/java/com/jiaojing/L011/Test.java
@@ -5,8 +5,8 @@ import org.springframework.context.ApplicationContext;
 
 public class Test {
     public static void main(String[] args){
-        //按配置文件读取顺序，如果允许覆盖，那么打印I'am ClassB！覆盖原来的I'am ClassA
-        ApplicationContext applicationContext = new CustomApplicationContext(new String[]{"same-name-beans-a.xml","same-name-beans-b.xml"});
+
+    	ApplicationContext applicationContext = new CustomApplicationContext(new String[]{"same-name-beans-a.xml","same-name-beans-b.xml"});
 
     }
 }
